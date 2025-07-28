322da264650a6897429f8f698290e0dbba404a32
Thilina Ruwan <info.inspirelk@gmail.com>
2025-07-28 09:46:24 +0000

    keep hero ane next secrion and remove other sections form the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@ import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 import Hero from '@/components/sections/hero';
 import About from '@/components/sections/about';
-import Services from '@/components/sections/services';
-import Projects from '@/components/sections/projects';
-import Contact from '@/components/sections/contact';
 
 export default function Home() {
   return (
@@ -14,12 +11,6 @@ export default function Home() {
         <Hero />
         <div id="about" />
         <About />
-        <div id="services" />
-        <Services />
-        <div id="projects" />
-        <Projects />
-        <div id="contact" />
-        <Contact />
       </main>
       <Footer />
     </div>
