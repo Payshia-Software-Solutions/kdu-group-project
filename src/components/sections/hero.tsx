68b9743f2b22dad51094b400cc3fb181a8ffd57f
Thilina Ruwan <info.inspirelk@gmail.com>
2025-07-28 12:46:34 +0000

    in the mobile screens need oonly two rows to display keep others but in

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -15,7 +15,7 @@ const sectors = [
 
 export default function Hero() {
   return (
-    <section id="home" className="relative w-full flex items-center justify-center py-8 md:min-h-[calc(100vh-7rem)]">
+    <section id="home" className="relative w-full flex items-center justify-center py-8 min-h-[calc(100vh-20rem)] md:min-h-[calc(100vh-7rem)]">
       <video
         autoPlay
         loop
