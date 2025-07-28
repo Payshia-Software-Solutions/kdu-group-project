0c232b0ae2635745ee4151e6901c5a4ea98a1a5c
Thilina Ruwan <info.inspirelk@gmail.com>
2025-07-28 09:32:04 +0000

    need more visibility tis too dak with top layer

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -25,7 +25,7 @@ export default function Hero() {
       >
         <source src="https://content-provider.payshia.com/kdu-group/hero-video.mp4" type="video/mp4" />
       </video>
-      <div className="absolute inset-0 bg-black/60 z-10"></div>
+      <div className="absolute inset-0 bg-black/50 z-10"></div>
       <div className="relative z-20 container mx-auto px-4 md:px-6 h-full">
         <div className="grid md:grid-cols-2 h-full items-center">
           <div className="text-white text-left">
